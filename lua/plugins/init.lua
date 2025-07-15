fedd1dd7c67abcfeeb70fbd0db23cb0c098fbe14
NILBOGtheSavior <naztechmedia@gmail.com>
2025-07-15 14:49:44 -0500

    Updated configuration and filestructure

---
+++ lua/plugins/init.lua
@@ -0,0 +1,18 @@
+-- You can add your own plugins here or in other files in this directory!
+--  I promise not to create any merge conflicts in this directory :)
+--
+-- See the kickstart.nvim README for more information
+
+return {
+  require 'plugins.debug',
+  require 'plugins.indent_line',
+  require 'plugins.autopairs',
+  require 'plugins.neo-tree',
+  require 'plugins.gitsigns',
+  {
+    'numToStr/Comment.nvim',
+    opts = {
+      -- add any options here
+    },
+  },
+}

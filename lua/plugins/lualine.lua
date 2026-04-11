ded5ca5d56fbe747d1f88175ecff203c07e2a995
NILBOGtheSavior <naztechmedia@gmail.com>
2026-04-10 20:07:38 -0500

    Updated separator

--- lua/plugins/lualine.lua
+++ lua/plugins/lualine.lua
@@ -7,7 +7,7 @@ return {
 		options = {
 			icons_enabled = true,
 			theme = "auto",
-			component_separators = { left = "╲ ", right = " ╱" },
+			component_separators = { left = "", right = "" },
 			section_separators = { left = "", right = "" },
 			disabled_filetypes = {
 				statusline = {},
